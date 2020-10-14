a616d2b906cfe28aa648f25545cdead1f2ca0741
olamide oladinni <olajide1987@gmail.com>
2020-10-13 19:39:22 -0500

    add files

--- employees/_init_.py
+++ employees/_init_.py
@@ -1,1 +1,1 @@
-from .request import get_single_employee, get_all_employees
+from .request import get_single_employee, get_all_employees, create_employee, delete_employee, update_employee
--- employees/request.py
+++ employees/request.py
@@ -25,3 +25,34 @@ def get_single_employee(id):
             requested_employee = employee
 
     return requested_employee
+
+def create_employee(employee):
+    max_id = EMPLOYEES[-1]["id"]
+
+    new_id = max_id + 1
+
+    employee["id"] = new_id
+
+    EMPLOYEES.append(employee)
+
+    return employee
+
+def delete_employee(id):
+    employee_index = -1
+
+    for index, employee in enumerate(EMPLOYEES):
+        if employee["id"] == id:
+            employee_index = index
+           
+
+    if employee_index >= 0:
+        EMPLOYEES.pop(employee_index)
+
+def update_employee(id, new_employee):
+    # Iterate the employeeS list, but use enumerate() so that
+    # you can access the index value of each item.
+    for index, employee in enumerate(EMPLOYEES):
+        if employee["id"] == id:
+            # Found the employee. Update the value.
+            EMPLOYEES[index] = new_employee
+            break
